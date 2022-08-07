e2d0657f5840661f39e7f67b8146a1740def9d3f
Joseph L <vipercar233@gmail.com>
2022-08-07 14:48:34 -0500

    create css and eventlistener

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,62 @@
 // 3. When game is over and prompt the initials name to storage with score.
 //      a. display the list of scores. (localStorage)
 
+var questions = [
+  {
+    question: "what is a question?",
+    answers: {
+                a: "A question mark",
+                b: "What?",
+                c: "I am asking the same question.",
+                d: "none of above"
+    },
+    correct: "a",
+            },
+    {
+    question: "How many languages is Javascript?",
+    answers: {
+                a: "JS is languages of many",
+                b: "JS is a language itself",
+                c: "Three",
+                d: "2! Java is a language so is script. Combine together 2 languages"
+    },
+    correct: "b",
+            },
+    {
+    question: "is HTML a programming language?",
+    answers: {
+                a: "Pfft, HTML is not necessary to be named programming language!",
+                b: "Yes!",
+                c: "Maybe",
+                d: "none of above"
+    },
+    correct: "d",
+            },
+            {
+    question: "Bonus points! Is Earth flat?",
+    answers: {
+                a: "Yes",
+                b: "No",
+                c: "Earth has shape?",
+                d: "None of above"
+    },
+    correct: "a",
+            },
+    ];
+
 var timerEl = document.getElementById('timer');
-var highScoreEl = document.querySelector('#high-score')
+var highScoreEl = document.querySelector('#high-score');
+var quizEl = document.getElementById('quiz');
 let startBtn = document.getElementById("start-btn");
+var resultsEl = document.getElementById('results');
+var submitEl = document.getElementById('submit');
+var answerEls = document.getElementById('.answer');
+var questionEl = document.getElementById('question');
+var a_text = document.getElementById('a_text');
+var b_text = document.getElementById('b_text');
+var c_text = document.getElementById('c_text');
+var d_text = document.getElementById('d_text');
+
 
 function timer() {
   var timeLeft = 60;
@@ -39,48 +92,53 @@ function timer() {
     alert("Time's up!");
   }
 
-startBtn.addEventListener('click', event => {
-  timer();
+  let currentQuiz = 0;
+  let score = 0;
+
+  loadQuiz();
+
+  function loadQuiz() {
+    deselectAnswers()
+
+    var currentQuizData = questions[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
   }
-);
-var questions = [
-  {
-    question: "what is a question?",
-    answers: {
-                a: "A question mark",
-                b: "What?",
-                c: "I am asking the same question.",
-                d: "none of above"
-    },
-    correctAnswer: "a"
-            },
-    {
-    question: "How many languages is Javascript?",
-    answers: {
-                a: "JS is languages of many",
-                b: "JS is a language itself",
-                c: "Three",
-                d: "2! Java is a language so is script. Combine together 2 languages"
-    },
-    correctAnswer: "b"
-            },
-    {
-    question: "is HTML a programming language?",
-    answers: {
-                a: "Pfft, HTML is not necessary to be named programming language!",
-                b: "Yes!",
-                c: "Maybe",
-                d: "none of above"
-    },
-    correctAnswer: "d"
-            },
-            {
-    question: "Bonus points! Is Earth flat?",
-    answers: {
-                a: "Yes",
-                b: "No",
-                c: "Earth has shape?"
-    },
-    correctAnswer: "b"
-            },
-          ]
+
+  function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+  }
+
+  function getSelected() {
+    let answer
+
+    answerEls.forEach(answerEl => {
+      if(answerEl.checked) {
+      }
+    })
+    return answer
+  }
+
+  submitEl.addEventListener('click', () => {
+    var answer = getSelected()
+    if(answer) {
+      if (answer === questions[currentQuiz].correct) {
+        score++
+      }
+      currentQuiz++
+
+      if (currentQuiz < questions.length) {
+        loadQuiz();
+      } else {
+        quizEl.innerHTML = `
+        <h2>You answered ${score}/${questions.length} question correctly</h2>
+        
+        <button onclick="location.reload()">Reload</button>
+        `
+      }
+    }
+  })
